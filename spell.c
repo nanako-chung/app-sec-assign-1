4ad128a019ae9a92fdd4834f2db252251ca88d5d
nanako-chung <nanako.s.chung@gmail.com>
2019-09-22 20:12:13 -0400

    finished writing functions- on to testing

--- spell.c
+++ spell.c
@@ -1,68 +1,101 @@
 #include "dictionary.h"
 #include "dictionary.c"
-#include <string.h>
 
-// checks if a word is correctly spelled
 int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]) {
-    return 0;
+	int count = 0;
+
+	//check if file exists
+	if (fp != NULL && hashtable != NULL) {
+
+		// maximum length for a word is 45 * 1 byte
+		char line[LENGTH + 1];
+
+		// read word one by one
+		while (fgets(line, sizeof(line), fp) != NULL) {
+
+            // use check_word function
+            if (!check_word(line, hashtable)) {
+                misspelled[count] = line;
+                count += 1;
+            }
+		}
+
+		//close file
+		fclose(fp);
+
+        // since count kept track of index we have to add 1 to get the length
+		return count + 1;
+	} 
+
+	return count;
 }
 
 bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]) {
-    
-    // open the file for reading
-    FILE * in_file = fopen(dictionary_file, "r" );
+	
+	// open the file for reading
+	FILE * in_file = fopen(dictionary_file, "r" );
 
-    //check if file exists
-    if (in_file != NULL && hashtable != NULL) {
+	//check if file exists
+	if (in_file != NULL && hashtable != NULL) {
 
-        // maximum length for a word is 45 * 1 byte
-        char line[45];
+		// maximum length for a word is 45 * 1 byte
+		char line[LENGTH + 1];
 
-        // read word one by one
-        while (fgets(line, sizeof(line), in_file) != NULL) {
-            int index = hash_function(line);
-            node * head = hashtable[index];
-            node * newNode = malloc(sizeof(node));
-            if (newNode == NULL) {
-                return false;
-            }
-            
-            for (int i = 0; i < strlen(line); i++) {
-                newNode->word[i] = line[i];
-            }
-            
-            newNode->next = NULL;
+		// read word one by one
+		while (fgets(line, sizeof(line), in_file) != NULL) {
+			int index = hash_function(line);
+			node * head = hashtable[index];
+			node * newNode = malloc(sizeof(node));
+			if (newNode == NULL) {
+				return false;
+			}
+			
+			for (int i = 0; i < strlen(line); i++) {
+				newNode->word[i] = line[i];
+			}
+			
+			newNode->next = NULL;
 
-            // if there is nothing stored at that index yet, add it
-            if (head == NULL) {
-                head = newNode;
-            } else {
-                node * current = head;
-                while (current->next != NULL) {
-                    current = current->next;
-                }
-                current->next = newNode;
-            }
-        }
+			// if there is nothing stored at that index yet, add it
+			if (head == NULL) {
+				head = newNode;
+			} else {
+				node * current = head;
+				while (current->next != NULL) {
+					current = current->next;
+				}
+				current->next = newNode;
+			}
+		}
 
-        //close file
-        fclose(in_file);
-        return true;
-    } 
+		//close file
+		fclose(in_file);
+		return true;
+	} 
 
-    return false;
+	return false;
 }
 
-// ties above funcs together
-// word param is file to pointer containing lines of words separated by spaces, punctuation, etc.
 bool check_word(const char* word, hashmap_t hashtable[]) {
-    return false;
+    int index = hash_function(word);
+    node * head = hashtable[index];
+
+    if (head != NULL) {
+        node * current = head;
+        while (current != NULL) {
+            if (strcmp(current->word, word) == 0) {
+                return true;
+            }
+        }
+    }
+
+	return false;
 }
 
 // test main - DOES NOT NEED TO BE SUBMITTED
 int main() {
-    // printf(check_words());
-    load_dictionary("wordlist.txt", hashtable);
-    // printf(check_word());
-    return 0;
+	char * misspelled[MAX_MISSPELLED];
+	printf("%i\n", check_words(fopen("test_worlist.txt", "r"), hashtable, misspelled));
+	load_dictionary("wordlist.txt", hashtable);
+	return 0;
 }
