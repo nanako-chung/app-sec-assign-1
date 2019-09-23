537a1c916d83b573a2699d21f194d94f4d0fb7aa
nanako-chung <nanako.s.chung@gmail.com>
2019-09-23 12:43:47 -0400

    found bug in check_words function

--- spell.c
+++ spell.c
@@ -2,7 +2,7 @@
 #include "dictionary.c"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <strings.h>
 
 int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]) {
 	int count = 0;
@@ -13,16 +13,32 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]) {
 		// maximum length for a word is 45 * 1 byte
 		char line[LENGTH + 1];
 
+        char delimiter[] = " \t\r\n\v\f.,";
+
+        // change size?
+        char * line_data[256];
+        int i = 0;
+        
 		// read word one by one
 		while (fgets(line, sizeof(line), fp) != NULL) {
+            
+            line_data[i] = strtok(line, delimiter); 
+            while (line_data[i] != NULL) {
 
-            // use check_word function
-            if (!check_word(line, hashtable)) {
-                misspelled[count] = line;
-                count += 1;
+                for (int j = 0; j < strlen(line_data[i]); j++) {
+                    line_data[i][j] = tolower(line_data[i][j]);
+                }
+
+                if (!check_word(line_data[i], hashtable)) {
+                    misspelled[count] = line_data[i];
+                    count += 1;
+                }
+
+                i++;
+                line_data[i] = strtok(NULL, delimiter);
             }
 		}
-
+        
 		//close file
 		fclose(fp);
 
@@ -34,7 +50,11 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]) {
 }
 
 bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]) {
-	
+	// initialize hashtable with nulls
+    for (int i = 0; i < HASH_SIZE; i++) {
+        hashtable[i] = NULL;
+    }
+
 	// open the file for reading
 	FILE * in_file = fopen(dictionary_file, "r" );
 
@@ -46,24 +66,26 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]) {
 
 		// read word one by one
 		while (fgets(line, sizeof(line), in_file) != NULL) {
-			int index = hash_function(line);
-			node * head = hashtable[index];
 			node * newNode = malloc(sizeof(node));
 			if (newNode == NULL) {
 				return false;
 			}
 			
 			for (int i = 0; i < strlen(line); i++) {
-				newNode->word[i] = line[i];
+                if (line[i] != '\n') {
+                    newNode->word[i] = tolower(line[i]);
+                }
 			}
-			
+
+			int index = hash_function(newNode->word);
+
 			newNode->next = NULL;
 
 			// if there is nothing stored at that index yet, add it
-			if (head == NULL) {
-				head = newNode;
+            if (hashtable[index] == NULL) {
+				hashtable[index] = newNode;
 			} else {
-				node * current = head;
+				node * current = hashtable[index];
 				while (current->next != NULL) {
 					current = current->next;
 				}
@@ -81,24 +103,23 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]) {
 
 bool check_word(const char* word, hashmap_t hashtable[]) {
     int index = hash_function(word);
-    node * head = hashtable[index];
-
-    if (head != NULL) {
-        node * current = head;
-        while (current != NULL) {
-            if (strcmp(current->word, word) == 0) {
-                return true;
-            }
+    node * current = hashtable[index];
+    while (current != NULL) {
+        if (strcmp(current->word, word) == 0) {
+            return true;
         }
+
+        current = current->next;
     }
 
 	return false;
 }
 
 // test main - DOES NOT NEED TO BE SUBMITTED
-// int main() {
-// 	char * misspelled[MAX_MISSPELLED];
-// 	printf("%i\n", check_words(fopen("test_worlist.txt", "r"), hashtable, misspelled));
-// 	load_dictionary("wordlist.txt", hashtable);
-// 	return 0;
-// }
+int main() {
+	if (load_dictionary("wordlist.txt", hashtable)) {
+        char * misspelled[MAX_MISSPELLED];
+        printf("%i\n", check_words(fopen("test1.txt", "r"), hashtable, misspelled));
+    }
+	return 0;
+}
