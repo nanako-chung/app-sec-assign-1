8be13df84facf1b3ab4a44cee3e34c15a4e49202
nanako-chung <nanako.s.chung@gmail.com>
2019-09-30 01:06:28 -0400

    edited check_word function

--- spell.c
+++ spell.c
@@ -5,14 +5,14 @@
 #include <ctype.h>
 
 bool trim(char* word) {
-	if (strlen(word) == 0 || word == NULL) return false;
 	while (!isalnum(word[strlen(word) - 1]) || !isalnum(word[0])) {
-		if (strlen(word) == 0) return false;
-		if (!isalnum(word[0])) memmove(word, word+1, strlen(word)); 
+		if (strlen(word) == 0) break;
+		if (!isalnum(word[0])) memmove(word, word + 1, strlen(word)); 
 		if (!isalnum(word[strlen(word) - 1])) word[strlen(word) - 1] = 0;
 	}
-
+	
 	// faulty detection
+	if (strlen(word) == 0) return false;
 	if (strcmp(word, "'") == 0) return false;
 	return true;
 }
@@ -36,31 +36,9 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]) {
 		char *individual_word = strtok(word, delimiter); 
 		while (individual_word != NULL) {
 			if (trim(individual_word)) {
-				char correct_word[LENGTH + 1];
-				strcpy(correct_word, individual_word);
-
-				int numeric_flag = 0;
-				int num_commas = 0;
-				int num_decimals = 0;
-				for (int i = 0; i < strlen(individual_word); i++) {
-					
-					// lower case the word because everything in dictionary is lower case
-					individual_word[i] = tolower(individual_word[i]);
-
-					if (isdigit(individual_word[i])) numeric_flag++;
-					if (individual_word[i] == ',') num_commas++;
-					if (individual_word[i] == '.') num_commas++;
-				}
-
-				// all numeric with commas, don't add to misspelled - consider th,st,nd,rd and fractions (e.g. 1/10)
-				if (numeric_flag == (strlen(individual_word) - num_commas) || numeric_flag == (strlen(individual_word) - num_decimals)) {
-					individual_word = strtok(NULL, delimiter); 
-					continue;
-				}
-
 				if (!check_word(individual_word, hashtable)) {
-					ptr[count] = (char*) malloc(sizeof(correct_word));
-					strcpy(ptr[count], correct_word);
+					ptr[count] = (char*) malloc(sizeof(individual_word));
+					strcpy(ptr[count], individual_word);
 					count += 1;
 				}
 			}
@@ -74,7 +52,6 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]) {
 
 	//close file
 	fclose(fp);
-	free(ptr);
 	return count;
 }
 
@@ -96,21 +73,19 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]) {
 	// maximum length for a word is 45 * 1 byte
 	char line[LENGTH + 1];
 	int count = 0;
-	node * newNode = (node *) malloc(sizeof(node));
-	if (newNode == NULL) {
-		return false;
-	}
 
 	// read word one by one
 	while (fgets(line, sizeof(line), in_file) != NULL) {
-		int i;
-		for (i = 0; i < strlen(line); i++) {
+		node * newNode = malloc(sizeof(node));
+		if (newNode == NULL) {
+			return false;
+		}
+		
+		for (int i = 0; i < strlen(line); i++) {
 			if (line[i] != '\n' && line[i]) {
 				newNode->word[i] = tolower(line[i]);
 			}
 		}
-
-		newNode->word[i] = '\0';
 
 		int index = hash_function(newNode->word);
 
@@ -135,15 +110,32 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]) {
 
 	//close file
 	fclose(in_file);
-	free(newNode);
 	return true;
 }
 
 bool check_word(const char* word, hashmap_t hashtable[]) {
-	int index = hash_function(word);
+	char individual_word[LENGTH + 1];
+	memcpy(individual_word, word, LENGTH + 1);
+	int numeric_flag = 0;
+	int num_commas = 0;
+	int num_decimals = 0;
+	for (int i = 0; i < strlen(word); i++) {
+		
+		// lower case the word because everything in dictionary is lower case
+		individual_word[i] = tolower(word[i]);
+
+		if (isdigit(word[i])) numeric_flag++;
+		if (word[i] == ',') num_commas++;
+		if (word[i] == '.') num_commas++;
+	}
+
+	// all numeric with commas, don't add to misspelled - consider th,st,nd,rd and fractions (e.g. 1/10)
+	if (numeric_flag == (strlen(individual_word) - num_commas) || numeric_flag == (strlen(individual_word) - num_decimals)) return true;
+
+	int index = hash_function(individual_word);
 	node * current = hashtable[index];
-	while (current != NULL) {
-		if (strcmp(current->word, word) == 0) {
+	while (current != NULL) {	
+		if (strcmp(current->word, individual_word) == 0) {
 			return true;
 		}
 
